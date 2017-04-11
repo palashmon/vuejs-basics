4bdd6cbaa9969ffe4eba28e4f8907634cdc923d1
Palash Mondal <palashm@live.in>
2017-04-11 19:58:58 +0530

    Added vuejs chapter 18 example codes

---
+++ 18-ajax-request-axios/app.js
@@ -0,0 +1,48 @@
+// Creating a Vue Instance with new Vue()
+new Vue({
+
+	el: '#root',
+
+	data: {
+		shown: false,
+		loading: false,
+		username: 'wesbos',
+		avatar_url: '',
+		blog: '',
+		followers: '',
+		following: '',
+		public_repos: '',
+		email: ''
+	},
+
+	methods: {
+		getUserData() {
+
+			var self = this;
+			self.loading = true;
+
+			setTimeout(() => {
+
+			// Performing a GET request using axios
+			axios.get('https://api.github.com/users/' + this.username)
+				.then(function(response) {
+					self.loading = false;
+					console.log(response.data); // ex.: { user: 'Your User'}
+					
+					// Set the required params
+					self.avatar_url = response.data.avatar_url;
+					self.blog = response.data.blog;
+					self.followers = response.data.followers;
+					self.following = response.data.following;
+					self.public_repos = response.data.public_repos;
+					self.email = response.data.email;
+					self.shown = true;
+
+				}).catch(function(error) {
+					console.log(error);
+				});
+			}, 1200); // run after 2 sec	
+		}
+	}
+
+})
